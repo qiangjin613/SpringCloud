9a193e212a6f7bf4e1ad0af9dab8f1c61349a645
qiangjin613 <qiangjin613@163.com>
2022-04-10 17:22:05 +0800

    添加使用 Open Feign 的 Order 消费模块

---
+++ cloud-consumer-feign-order80/src/main/java/com/qiangjin/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.qiangjin.springcloud.controller;
+
+import com.qiangjin.springcloud.entity.CommonResult;
+import com.qiangjin.springcloud.entity.PaymentDo;
+import com.qiangjin.springcloud.service.PaymentFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer/order")
+public class OrderFeignController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/get/{id}")
+    public CommonResult<PaymentDo> getPayment(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/qiangjin/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,25 @@
+package com.qiangjin.springcloud.service;
+
+import com.qiangjin.springcloud.entity.CommonResult;
+import com.qiangjin.springcloud.entity.PaymentDo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@Component
+@FeignClient("CLOUD-PROVIDER-PAYMENT-SERVICE")
+@RequestMapping("/payment")
+public interface PaymentFeignService {
+
+    @PostMapping("/create")
+    CommonResult create(@RequestBody PaymentDo paymentDo);
+
+    @GetMapping("/get/{id}")
+    CommonResult<PaymentDo> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping("/discovery")
+    Object discovery();
+
+    @GetMapping("/serverPort")
+    String getServerPort();
+}
